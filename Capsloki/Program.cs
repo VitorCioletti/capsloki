f90f97d52fa996d3bec5aabb9f98c0f3de1d5097
VitorCioletti <ciolettiv@gmail.com>
2018-05-25 10:03:21 -0300

    Update Program.cs

--- Capsloki/Program.cs
+++ Capsloki/Program.cs
@@ -27,8 +27,8 @@
 
 			Action turnCapsLockOff = () =>
 			{
-				keybd_event(0x14, 0x45, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
-				keybd_event(0x14, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, (UIntPtr)0);
+				keybd_event(CAPSLOCK, 0x45, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
+				keybd_event(CAPSLOCK, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, (UIntPtr)0);
 			};
 
 			Action execute = () => 
